0e1c1a8002cc10cf2570f7f18a185fc5f6d0a32a
Bob <1004707367@qq.com>
2023-12-15 15:10:20 +0800

    update

---
+++ ch01/1_23.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "include/Sales_item.h"
+using namespace std;
+
+int main()
+{
+
+    Sales_item currItem, valItem;
+    if (cin >> currItem)
+    {
+        int cnt = 1;
+        while (cin >> valItem)
+        {
+            if (valItem.isbn() == currItem.isbn())
+            {
+                ++cnt;
+            }
+            else
+            {
+                cout << currItem << " occurs " << cnt << " times " << endl;
+                currItem = valItem;
+                cnt = 1;
+            }
+        }
+        cout << currItem << " occurs " << cnt << " times " << endl;
+    }
+    return 0;
+}
